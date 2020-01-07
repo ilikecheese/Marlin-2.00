e6a8902bff9f09e1468bdd6251cdf5cb8d2d80f9
ilikecheese <nick@oldbranch.com>
2020-01-06 23:39:40 -0600

    Set fan pins in config adv

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -399,7 +399,7 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN -1
+#define E0_AUTO_FAN_PIN PE5  //check 'pins_BTT_SKR_PRO_V1_1.h' for fan pins
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
@@ -407,7 +407,7 @@
 #define E5_AUTO_FAN_PIN -1
 #define CHAMBER_AUTO_FAN_PIN -1
 
-#define EXTRUDER_AUTO_FAN_TEMPERATURE 50
+#define EXTRUDER_AUTO_FAN_TEMPERATURE 80
 #define EXTRUDER_AUTO_FAN_SPEED 255   // 255 == full speed
 #define CHAMBER_AUTO_FAN_TEMPERATURE 30
 #define CHAMBER_AUTO_FAN_SPEED 255
@@ -419,7 +419,7 @@
  * The multiplexer is automatically switched at tool-change.
  * Set FANMUX[012]_PINs below for up to 2, 4, or 8 multiplexed fans.
  */
-#define FANMUX0_PIN -1
+#define FANMUX0_PIN PC8 //check 'pins_BTT_SKR_PRO_V1_1.h' for fan pins
 #define FANMUX1_PIN -1
 #define FANMUX2_PIN -1
 
